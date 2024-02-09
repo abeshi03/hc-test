fa42e6fcfd3437ea19f0f12934d9bad58f5c7412
石川沙季 <saki.ishikawa@ishikawasakinoMacBook-Pro.local>
2024-02-09 17:13:04 +0900

    feat: ユーザー一覧作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import { FC } from "react";
 import Link from "next/link";
 import { Layout } from "../components/Layout";
-import s from "./style.module.sass"
+import s from "./style.module.sass";
+
 
 const Top: FC = () => {
   return (
     <Layout>
       <p>トップページです</p>
       <div className={s.links}>
+        <Link href="/users">ユーザー一覧ページへ</Link>
         <Link href="/mock">モックページへ</Link>
         <Link href="/count">カウントページ</Link>
       </div>
