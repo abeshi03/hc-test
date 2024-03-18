7e1eaad69f66255be059da1a6281a98652728069
saki-pg <saki.ishikawa@MacBook-Pro-2.local>
2024-03-18 17:17:01 +0900

    FormAssignment-2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,19 @@
-import { FC } from "react";
-import Link from "next/link";
-import { Layout } from "../components/Layout";
-import s from "./style.module.sass";
-
+import { FC } from "react"
+import Link from "next/link"
+import { Layout } from "../components/Layout"
+import s from "./style.module.sass"
 
 const Top: FC = () => {
   return (
     <Layout>
-      <p>トップページです</p>
+      <h2>トップページです</h2>
       <div className={s.links}>
-        <Link href="/users">ユーザー一覧ページへ</Link>
-        <Link href="/mock">モックページへ</Link>
-        <Link href="/count">カウントページ</Link>
-        <Link href="/tests">テストページ</Link>
-        <Link href="/form">フォームページ</Link>
+        <Link href="/users">ユーザー一覧ページへ</Link><br/>
+        <Link href="/mock">モックページへ</Link><br/>
+        <Link href="/count">カウントページ</Link><br/>
+        <Link href="/tests">テストページ</Link><br/>
+        <Link href="/form">フォームページ</Link><br/>
+        <Link href="/form-2">フォームページ2</Link><br/>
       </div>
     </Layout>
   )
